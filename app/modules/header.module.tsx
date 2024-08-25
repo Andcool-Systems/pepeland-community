0f9d159c5496a2b674ec2c4bdc4233500b3052e4
Andcool-Systems <drolll2007@gmail.com>
2024-08-26 00:23:52 +0300

    initial commit

---
+++ app/modules/header.module.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import NextImage from 'next/image';
+import NextLink from 'next/link';
+import styles from '@/app/styles/header.module.css';
+import { CSSTransition } from 'react-transition-group';
+import { useState } from 'react';
+
+const Header = () => {
+    const [expanded, setExpanded] = useState<boolean>(false);
+
+    return (
+        <>
+            <CSSTransition
+                in={expanded}
+                timeout={200}
+                classNames={{
+                    enter: styles.blur_enter,
+                    enterActive: styles.blur_enter_active,
+                    exit: styles.blur_exit,
+                    exitActive: styles.blur_exit_active,
+                }}
+                unmountOnExit>
+                <div className={styles.blur} />
+            </CSSTransition>
+
+            <CSSTransition
+                in={expanded}
+                timeout={200}
+                classNames={{
+                    enter: styles['menu-enter'],
+                    enterActive: styles['menu-enter-active'],
+                    exit: styles['menu-exit'],
+                    exitActive: styles['menu-exit-active'],
+                }}
+                unmountOnExit>
+                <div className={styles.menu}>
+                    <div className={styles.title_side}>
+                        <NextImage className={styles.exit_menu} src='/static/icons/plus.svg' alt='' width={32} height={32} onClick={() => setExpanded(false)} />
+                        <div className={styles.title_side_2}>
+                            <h1>Pepeland Community</h1>
+                            <NextImage src='/static/icons/icon.svg' alt='' width={25} height={25} />
+                        </div>
+                    </div>
+                    <div className={styles.pages}>
+                        <NextLink href=''>Проекты <NextImage src='/static/icons/stack.svg' alt='' width={25} height={25} /></NextLink>
+                        <NextLink href=''>Правила <NextImage src='/static/icons/book.svg' alt='' width={25} height={25} /></NextLink>
+                        <NextLink href=''>Репорты <NextImage src='/static/icons/flag.svg' alt='' width={25} height={25} /></NextLink>
+                    </div>
+                </div>
+            </CSSTransition>
+            <header className={styles.header}>
+                <div className={styles.child}>
+                    <div className={styles.title}>
+                        <NextLink href='/'><NextImage src='/static/icons/icon.svg' alt='' width={55} height={55} className={styles.main_icon} /></NextLink>
+                        <h1>Pepeland Community</h1>
+                    </div>
+                    <div className={styles.links}>
+                        <NextLink href=''>Проекты</NextLink>
+                        <NextLink href=''>Правила</NextLink>
+                        <NextLink href=''>Репорты</NextLink>
+                    </div>
+                    <NextImage src='/static/icons/burger-menu.svg'
+                        alt=''
+                        className={styles.burger}
+                        width={30}
+                        height={30}
+                        onClick={() => { setExpanded(true) }}
+                    />
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,19 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import style from '@/app/styles/page.module.css';
+import { Manrope } from "next/font/google";
+import NextLink from 'next/link';
+import NextImage from 'next/image';
+const manrope = Manrope({ subsets: ["latin", "cyrillic"] });
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <div className={style.main}>
+                <div className={style.animated}>
+                    <h1 className={manrope.className}>Общий Домен</h1>
+                    <p>Для сайтов, связанных с Пепелендом</p>
+                    <NextLink href='/rules' className={style.button}><NextImage src='/static/icons/book.svg' alt='' width={20} height={20} /> Правила</NextLink>
+                </div>
+            </div>
+        </main>
+    );
 }
