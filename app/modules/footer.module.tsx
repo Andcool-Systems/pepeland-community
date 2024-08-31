2b4902dd78ef5dd046cbad2f4f650fa37014892c
Andcool-Systems <drolll2007@gmail.com>
2024-08-31 11:58:23 +0300

    fixed firefox header & added footer into main page

---
+++ app/modules/footer.module.tsx
@@ -0,0 +1,11 @@
+import style from '@/app/styles/footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={style.footer}>
+            <p>Сайт pepeland.space не является официальной частью сети серверов PepeLand.</p>
+        </footer>
+    )
+}
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,22 @@
 import style from '@/app/styles/page.module.css';
 import { Manrope } from "next/font/google";
 import NextLink from 'next/link';
-import NextImage from 'next/image';
 import { IconStack } from '@tabler/icons-react';
+import Footer from './modules/footer.module';
 
 const manrope = Manrope({ subsets: ["latin", "cyrillic"] });
 
 export default function Home() {
     return (
-        <main>
-            <div className={style.main}>
+        <>
+            <main className={style.main}>
                 <div className={style.animated}>
                     <h1 className={manrope.className} style={{ textWrap: 'nowrap' }}>Общий Домен</h1>
                     <p>Для сайтов, связанных с Пепелендом</p>
                     <NextLink href='/projects' className={style.button}><IconStack width={22} height={22} /> Проекты</NextLink>
                 </div>
-            </div>
-        </main>
+            </main>
+            <Footer />
+        </>
     );
 }
